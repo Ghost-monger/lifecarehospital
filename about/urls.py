816c2a4019bae20439bb5b6ab847585c2572d006
Ghost-monger <emmanuelmusyoka898@gmail.com>
2026-02-19 13:40:18 +0300

    first commit

---
+++ about/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for lifehospital project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from about import views
+from about.views import appointment
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/', views.about, name='about.html'),
+    path('contact/', views.contact, name='contact.html'),
+    path('appointment/', appointment, name='appointment.html'),
+    path('departmentdetails/', views.department_details, name='department-details.html'),
+    path('departments/', views.departments, name='departments.html'),
+    path('doctors/', views.doctors, name='doctors.html'),
+    path('faq/', views.faq, name='faq.html'),
+    path('gallery/', views.gallery, name='gallery.html'),
+    path('index/', views.index, name='index.html'),
+    path('privacy/', views.privacy, name='privacy.html'),
+    path('terms/', views.terms, name='terms.html'),
+    path('servicedetails/', views.service_details, name='service-details.html'),
+    path('services/', views.services, name='services.html'),
+    path('starterpage/', views.starter_page, name='starter-page.html'),
+    path('testimonials/', views.testimonials, name='testimonials.html'),
+
+
+
+]
---
+++ about/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about (request):
+    return render(request, 'about.html')
+def appointment(request):
+    return render(request, 'appointment.html')
+def contact(request):
+    return render(request, 'contact.html')
+def department_details(request):
+    return render(request, 'department-details.html')
+def departments(request):
+    return render(request, 'departments.html')
+def doctors(request):
+    return render(request, 'doctors.html')
+def faq(request):
+    return render(request, 'faq.html')
+def gallery (request):
+    return render(request, 'gallery.html')
+def index(request):
+    return render(request, 'index.html')
+def privacy(request):
+    return render(request, 'privacy.html')
+def terms(request):
+    return render(request, 'terms.html')
+def service_details(request):
+    return render(request, 'service-details.html')
+def services(request):
+    return render(request, 'services.html')
+def starter_page(request):
+    return render(request, 'starter-page.html')
+def testimonials(request):
+    return render(request, 'testimonials.html')
